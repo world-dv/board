6865a437a0198e5a98a647cda5719105774ad48e
world-dv <goodforx0147@naver.com>
2025-03-16 17:39:54 +0900

    feat: db 속성 추가 및 좋아요 수 조회
    - 좋아요 수 추가
    - 후기 생성, 수정 시간 추가

--- board.py
+++ board.py
@@ -2,15 +2,21 @@ import streamlit as st
 import sqlite3
 
 # DB 연동 - db이름: example.db
-conn = sqlite3.connect('example.db')
+conn = sqlite3.connect('board/example.db') # + db의 위치 고정 필요 
 cursor = conn.cursor()
 
 # DB에 테이블 생성 - 속성: board_id(PK), 이름, 리뷰
+# + 테이블 이름 복수형으로 변경 board -> boards
+# + 생성 날짜(created_at), 수정(updated_at) 날짜 추가
+# + 컬럼 이름 수정 : 이름 -> board_name, 내용 -> comment 
 cursor.execute('''
-    CREATE TABLE IF NOT EXISTS board (
+    CREATE TABLE IF NOT EXISTS boards (
         board_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        이름 VARCHAR(255) NOT NULL,
-        리뷰 TEXT NOT NULL
+        board_name VARCHAR(255) NOT NULL,
+        comment TEXT NOT NULL,
+        likes INTEGER NOT NULL DEFAULT 0,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     )
 ''')
 # SQLlite는 commit을 꼭 해줘야한대~
@@ -35,7 +41,8 @@ def service_intro():
 # 후기 작성
 def new_geustbook():
     st.header("사용자 후기")
-        # 후기 작성 폼
+    
+    # 후기 작성 폼
     with st.form(key='review_form'):
         user_name = st.text_input("이름") # 이름 작성하는 칸 생성
         user_review = st.text_area("후기 작성") # 후기 작성 칸 생성
@@ -45,23 +52,27 @@ def new_geustbook():
     if submit_button:
         if user_name and user_review:
             st.success("소중한 후기 감사합니다")
-            cursor.execute("INSERT INTO board (이름, 리뷰) VALUES (?, ?)", (user_name, user_review)) #DB에 저장하기
+            cursor.execute("INSERT INTO boards (board_name, comment) VALUES (?, ?)", (user_name, user_review)) #DB에 저장하기
             conn.commit() # DB 변경 사항 저장
         else:
             st.error("이름과 후기를 모두 작성해 주세요.") # 이름이나 후기가 작성되지 않은 경우
 
-        # 제출된 후기 표시 (새로운 후기 먼저 표시)
+    # 제출된 후기 표시 (새로운 후기 먼저 표시)
     st.write("### 제출된 후기들")
 
-    cursor.execute("SELECT * FROM board") # DB에서 정보 가져오기
+    cursor.execute("SELECT * FROM boards") # DB에서 정보 가져오기
     all_review = cursor.fetchall() # DB 정보들 all_review 변수로 선언
 
-        # 제출된 리뷰 보기 & 삭제하기 & 편집하기
+    # 제출된 리뷰 보기 & 삭제하기 & 편집하기
     for idx, row in enumerate(all_review):
         review_id = row[0]
         name = row[1]  # 첫 번째 열은 '이름'
         review = row[2]  # 두 번째 열은 '리뷰'
+        likes = row[3] # + 좋아요 수
+
+        print(row) # + 조회 결과 표시 - 데이터 확인
         st.write(f"💛 {name}님: {review}")
+        st.write(f"좋아요 수: {likes}") # + 좋아요 표시 기능 추가
 
         left_column, right_column = st.columns(2) 
 
@@ -70,7 +81,7 @@ def new_geustbook():
 
     # 삭제 버튼 클릭 시 리뷰 삭제
         if delete_button:
-            cursor.execute("DELETE FROM board WHERE board_id = ?", (review_id,))
+            cursor.execute("DELETE FROM boards WHERE board_id = ?", (review_id, ))
             conn.commit()  # 변경사항을 DB에 적용
             st.write(f"{name}님의 리뷰가 삭제되었습니다.")
             st.rerun()  # 페이지 새로 고침
